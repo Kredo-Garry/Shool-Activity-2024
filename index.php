c78a1678ccb19f14ea8bc9ee1c02fd831ea80ab2
Kredo-Garry <kredoteacher.grodriguez@gmail.com>
2024-05-13 19:58:22 +0800

    I made some changes in index.php on May 13, 2024

--- index.php
+++ index.php
@@ -47,6 +47,7 @@
                     
                     <div class="d-grid gap-2">
                         <button type="submit" name="btn_submit" class="btn btn-dark">Submit</button>
+                        <p class="text-center small">Copyright @2024</p>
                     </div>
                     
                 </form>
